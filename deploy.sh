dba05b7d4b68d30bac7e8a3c1aee635a264c9500
mephisto <mephisto2120@gmail.com>
2020-05-31 10:30:12 +0200

    Travis config

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t mephisto2120/multi-client:latest -t mephisto2120/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t mephisto2120/multi-server:latest -t mephisto2120/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t mephisto2120/multi-worker:latest -t mephisto2120/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push mephisto2120/multi-client:latest
+docker push mephisto2120/multi-server:latest
+docker push mephisto2120/multi-worker:latest
+
+docker push mephisto2120/multi-client:$SHA
+docker push mephisto2120/multi-server:$SHA
+docker push mephisto2120/multi-worker:$SHA
+
+kubectl apply-f k8s
+kubectl set image deployments/server-deployment server=mephisto2120/multi-server:$SHA
+kubectl set image deployments/client-deployment client=mephisto2120/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=mephisto2120/multi-worker:$SHA
